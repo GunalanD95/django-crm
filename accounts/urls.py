4bee846b7ad47b5b79d4d97847d7cf7d47cc839c
unknown <guna19may2015@gmail.com>
2022-01-12 22:05:13 +0530

    added nav bar for home page

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('',views.home, name='home'),
+    path('customers', views.customers, name='customers'),
+    path('products', views.products, name='products'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'accounts/layout.html')
+
+
+def products(request):
+    return render(request, 'accounts/products.html')
+
+def customers(request):
+    return render(request, 'accounts/customers.html')
