190e75c6b656b1cad3da75c6ccd15c4775780cb9
unknown <guna19may2015@gmail.com>
2022-01-14 18:45:31 +0530

    added product,customer and sale order tables

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer , Product , SaleOrder
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(SaleOrder)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,50 @@
+from sre_parse import CATEGORIES
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    customer_pic = models.ImageField(upload_to='media/customer_pics', blank=True)
+    customer_name = models.CharField(max_length=50, null= True)
+    customer_email = models.CharField(max_length=200 , null= True)
+    customer_mobile = models.CharField(max_length=50 , null= True)
+    date_created = models.DateTimeField(auto_now_add=True , null= True)
+
+
+    def __str__(self):
+        return self.customer_name
+
+class Product(models.Model):
+
+    CATEGORIES = (
+        ('service', 'Service'),      
+        ('stockable', 'Stockable'),
+    )
+
+
+    product_name = models.CharField(max_length=200, null= True)
+    product_price = models.FloatField(null= True)
+    product_category = models.CharField(max_length=200, null= True , choices=CATEGORIES)
+    product_description = models.CharField(max_length=200, null= True)
+    date_created = models.DateTimeField(auto_now_add=True , null= True)
+
+
+class SaleOrder(models.Model):
+    STATUS = (
+        ('quotation', 'Quotation'),      
+        ('ordered', 'Ordered'),
+        ('pending', 'Pending'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled'),
+    )
+    sale_order_referencenumber = models.CharField(max_length=200, null= True)
+    #sale_order_customer = 
+    #sale_order_product = 
+    sale_order_quantity = models.IntegerField(null= True)
+    sale_order_total_price = models.FloatField(null= True)
+    status = models.CharField(max_length=200, null= True , choices=STATUS, default='quotation')
+    sale_order_date = models.DateTimeField(auto_now_add=True , null= True)
+
+    def __str__(self):
+        return self.sale_order_referencenumber
