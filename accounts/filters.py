4e9752f6f4f74aa3fbb6eb108ed888a661d9cb1a
unknown <guna19may2015@gmail.com>
2022-01-21 07:38:29 +0530

    filter orders

--- accounts/filters.py
+++ accounts/filters.py
@@ -5,4 +5,5 @@ from .models import *
 class OrderFilter(django_filters.FilterSet):
     class Meta:
         model = SaleOrder
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ['sale_order_customer','sale_order_product','status']
