6db124bfd95d56c8a03c52ad2c2452ddd9e6b7cb
unknown <guna19may2015@gmail.com>
2022-01-22 17:59:03 +0530

    reg user complete

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from django.urls.conf import include
-
 from . import views
 
 
--- users/views.py
+++ users/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import NewUserForm
 
 # Create your views here.
 
 
 def index(request):
-    return render(request,'users/login.html')
+    form = NewUserForm()
+    if request.method == 'POST':
+        print(request.POST,"REGISTER")
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            print("VALID")
+            user = form.save()
+            login(request,user)
+            messages.success(request,'You have been registered')
+            return redirect('home')
+        else:
+            messages.error(request, "Unsuccessful registration. Invalid information.")  
+    return render(request,'users/login.html',{'form':form})
+
