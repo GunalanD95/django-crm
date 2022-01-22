354a5013c53f0ad1f5d3b4cf3b7c1cb504efe26e
unknown <guna19may2015@gmail.com>
2022-01-22 18:26:10 +0530

    user-login auth using django.auth methods

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import  render, redirect
 from django.contrib.auth import login
 from django.contrib import messages
 from .forms import NewUserForm
+from django.contrib.auth.forms import AuthenticationForm #add this
+from django.contrib.auth import login, authenticate #add this
 
 # Create your views here.
 
@@ -9,15 +11,31 @@ from .forms import NewUserForm
 def index(request):
     form = NewUserForm()
     if request.method == 'POST':
-        print(request.POST,"REGISTER")
-        form = NewUserForm(request.POST)
-        if form.is_valid():
-            print("VALID")
-            user = form.save()
-            login(request,user)
-            messages.success(request,'You have been registered')
-            return redirect('home')
-        else:
-            messages.error(request, "Unsuccessful registration. Invalid information.")  
+        if 'register' in request.POST:
+            print(request.POST,"REGISTER")
+            form = NewUserForm(request.POST)
+            if form.is_valid():
+                print("VALID REG")
+                user = form.save()
+                login(request,user)
+                messages.success(request,'You have been registered')
+                return redirect('/')
+            else:
+                messages.error(request, "Unsuccessful registration. Invalid information.")
+        elif 'login' in request.POST:
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                print("VALID LOGIN")
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    messages.info(request, f"You are now logged in as {username}.")
+                    return redirect("/")
+                else:
+                    messages.error(request,"Invalid username or password.")
+            else:
+                messages.error(request,"Invalid username or password.")
     return render(request,'users/login.html',{'form':form})
 
