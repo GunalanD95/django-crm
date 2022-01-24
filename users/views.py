7f12d1c6e9346a842b17c757ce7fc07430f86366
unknown <guna19may2015@gmail.com>
2022-01-24 05:58:44 +0530

    fix auth

--- users/views.py
+++ users/views.py
@@ -6,45 +6,42 @@ from .forms import NewUserForm
 from django.contrib.auth.forms import AuthenticationForm #add this
 from django.contrib.auth import login, authenticate #add this
 from django.contrib.auth import logout
-
+from .decorators import unauthenticated_user
 # Create your views here.
 
-
+@unauthenticated_user
 def index(request):
-    if request.user.is_authenticated:
-        return redirect('/')
-    else:
-        form = NewUserForm()
-        if request.method == 'POST':
-            if 'register' in request.POST:
-                print(request.POST,"REGISTER")
-                form = NewUserForm(request.POST)
-                if form.is_valid():
-                    print("VALID REG")
-                    user = form.save()
-                    login(request,user)
-                    messages.success(request,'You have been registered')
-                    return redirect('/')
-                else:
-                    messages.error(request, "Unsuccessful registration. Invalid information.")
-            elif 'login' in request.POST:
-                form = AuthenticationForm(request, data=request.POST)
-                if form.is_valid():
-                    print("VALID LOGIN")
-                    username = form.cleaned_data.get('username')
-                    password = form.cleaned_data.get('password')
-                    user = authenticate(username=username, password=password)
-                    if user is not None:
-                        login(request, user)
-                        messages.info(request, f"You are now logged in as {username}.")
-                        return redirect("/")
-                    else:
-                        messages.error(request,"Invalid username or password.")
+    form = NewUserForm()
+    if request.method == 'POST':
+        if 'register' in request.POST:
+            print(request.POST,"REGISTER")
+            form = NewUserForm(request.POST)
+            if form.is_valid():
+                print("VALID REG")
+                user = form.save()
+                login(request,user)
+                messages.success(request,'You have been registered')
+                return redirect('/')
+            else:
+                messages.error(request, "Unsuccessful registration. Invalid information.")
+        elif 'login' in request.POST:
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                print("VALID LOGIN")
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    messages.info(request, f"You are now logged in as {username}.")
+                    return redirect("/")
                 else:
                     messages.error(request,"Invalid username or password.")
-        return render(request,'users/login.html',{'form':form})
+            else:
+                messages.error(request,"Invalid username or password.")
+    return render(request,'users/login.html',{'form':form})
 
-
+@unauthenticated_user
 def logout_view(request):
     logout(request)
     return redirect('/')
